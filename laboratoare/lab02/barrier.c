68c059d4176d87719758161594219fbe209d3fcf
David Bors <daviddvd267@gmail.com>
2022-10-10 14:44:05 +0300

    Lab2 partial solve

--- laboratoare/lab02/barrier.c
+++ laboratoare/lab02/barrier.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include "pthread_barrier_mac.h"
 
 #define NUM_THREADS 2
+pthread_barrier_t bariera;
 
 void *f(void *arg)
 {
 	int thread_id = *(int *)arg;
 
+	// pune bariera a.i. mereu sa se astepte executia bucatii acesteia de cod
+	int r = pthread_barrier_wait(&bariera);
 	if (thread_id == 1) {
 		printf("1\n");
 	}
@@ -26,6 +30,11 @@ int main(int argc, char **argv)
 	pthread_t threads[NUM_THREADS];
 	int arguments[NUM_THREADS];
 
+	r = pthread_barrier_init(&bariera, NULL, 2);
+	if (r) {
+		printf("Eroare la creearea barierei\n");
+	}
+
 	for (i = 0; i < NUM_THREADS; i++) {
 		arguments[i] = i;
 		r = pthread_create(&threads[i], NULL, f, &arguments[i]);
@@ -43,6 +52,11 @@ int main(int argc, char **argv)
 			printf("Eroare la asteptarea thread-ului %d\n", i);
 			exit(-1);
 		}
+	}
+
+	r = pthread_barrier_destroy(&bariera);
+	if (r) {
+		printf("eroare distrugere bariera\n");
 	}
 
 	return 0;
--- laboratoare/lab02/mutex.c
+++ laboratoare/lab02/mutex.c
@@ -5,10 +5,13 @@
 #define NUM_THREADS 2
 
 int a = 0;
+pthread_mutex_t zavor;
 
 void *f(void *arg)
 {
+	pthread_mutex_lock(&zavor);
 	a += 2;
+	pthread_mutex_unlock(&zavor);
 
 	pthread_exit(NULL);
 }
@@ -19,6 +22,11 @@ int main(int argc, char *argv[])
 	void *status;
 	pthread_t threads[NUM_THREADS];
 	int arguments[NUM_THREADS];
+	r = pthread_mutex_init(&zavor, NULL);
+	if (r) {
+		printf("Eroare la crearea mutex-ului %d\n", r);
+		exit(-1);
+	}
 
 	for (i = 0; i < NUM_THREADS; i++) {
 		arguments[i] = i;
@@ -40,6 +48,12 @@ int main(int argc, char *argv[])
 	}
 
 	printf("a = %d\n", a);
+
+	r = pthread_mutex_destroy(&zavor);
+	if (r) {
+		printf("Erooare la crearea mutex-ului %d\n", r);
+		exit(-1);
+	}
 
 	return 0;
 }
